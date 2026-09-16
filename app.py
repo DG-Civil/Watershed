9d99c7d0907a0047cc63ed5530a5277c8f2b92db
DG-Civil <dawit.ghebreyesust@gmail.com>
2026-09-16 17:42:57 -0500

    Watershed NLCD Multi-Year v3

--- app.py
+++ app.py
@@ -31,6 +31,7 @@ from rasterio.windows import Window
 from scipy.interpolate import griddata
 from shapely.geometry import box
 from streamlit_folium import st_folium
+import matplotlib as mpl
 
 st.set_page_config(
     page_title="Hydrology & CN Web Suite",
@@ -594,7 +595,8 @@ with tab1:
             g_min = st.session_state["global_min"]
             g_max = st.session_state["global_max"]
 
-            terrain_cmap = cm.get_cmap("terrain", 15)
+            #terrain_cmap = cm.get_cmap("terrain", 15)
+            terrain_cmap = mpl.colormaps['terrain'].resampled(15)
             hex_colors = [
                 mcolors.to_hex(terrain_cmap(i)) for i in np.linspace(0, 1, 15)
             ]
@@ -620,7 +622,9 @@ with tab1:
                             / (g_max - g_min)
                             * 255
                         ).clip(0, 255).astype(np.uint8)
-                        colored = cm.terrain(norm / 255.0) * 255
+                        
+                        #colored = cm.terrain(norm / 255.0) * 255
+                        colored = terrain_cmap(norm / 255.0) * 255
                         rgba_img = colored.astype(np.uint8)
 
                     rgba_img[..., 3] = np.where(dem["mask"], 160, 0)
